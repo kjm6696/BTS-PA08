6c51696f14ed6bb1f5a7115a55cda9f55a0a226b
kjm6696 <kjm6696@psu.edu>
2023-10-01 21:13:42 -0400

    delete comments added

--- src/main/java/BinarySearchTree.java
+++ src/main/java/BinarySearchTree.java
@@ -22,46 +22,47 @@ public class BinarySearchTree {
     }
 
     public Node delete(Node root, int index) {
-        if (this.root != null) {
-            if (index < root.data) {
-                root.left = delete(root.left, index);
-            } else if (index > root.data) {
-                root.right = delete(root.right, index);
-            } else {
-                if (root.right == null && root.left == null) {
-                    root.data = null;
-                } else if (root.right != null) {
-                    root.data = null;
-                    root.data = root.right.data;
-                    root.right.data = null;
-                } else if (root.left != null){
-                    root.data = null;
-                    root.data = root.left.data;
-                    root.left.data = null;
+        if (this.root != null) { // if root is not empty
+            if (index < root.data) { // if item wanted to delete is on left side of tree
+                root.left = delete(root.left, index); //keep running delete method until items is found
+            } else if (index > root.data) { // if item wanted to delete is on right side of tree
+                root.right = delete(root.right, index); //keep running delete method until items is found
+            } else { // item found now delete
+                if (root.right == null && root.left == null) { // if desired item has no children
+                    root.data = null; // delete node
+                } else if (root.right != null) { // if item has child on right (bigger)
+                    root.data = root.right.data;// child now takes parents spot
+                    root.right.data = null; // updated child spot to null
+                } else{ //if item has child on left (smaller but has no siblings)
+                    root.data = root.left.data; // child now takes parents spot
+                    root.left.data = null;// update child spot to null
                 }
             }
         }
-        return root;
-    }
-
-    public String inOrderTraversal(){
-        return this.inOrderTraversal(this.root);
-    }
-    private String inOrderTraversal(Node current){
-        StringBuilder stringBuilder = new StringBuilder();
-
-        if (current != null) {
-
-            stringBuilder.append(this.inOrderTraversal(current.left));
-
-            stringBuilder.append(current.data);
-            stringBuilder.append(" ");
-
-            stringBuilder.append(this.inOrderTraversal(current.right));
-
+        else { // if root == null
+            throw new IllegalArgumentException(); // throw exception
         }
-
-        return stringBuilder.toString();
+        return root;// return the new BST
     }
+
+//    public String inOrderTraversal(){
+//        return this.inOrderTraversal(this.root);
+//    }
+//    private String inOrderTraversal(Node current){
+//        StringBuilder stringBuilder = new StringBuilder();
+//
+//        if (current != null) {
+//
+//            stringBuilder.append(this.inOrderTraversal(current.left));
+//
+//            stringBuilder.append(current.data);
+//            stringBuilder.append(" ");
+//
+//            stringBuilder.append(this.inOrderTraversal(current.right));
+//
+//        }
+//
+//        return stringBuilder.toString();
+//    }
 
 }
